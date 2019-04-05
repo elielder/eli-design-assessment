6cf1c9cdfb81c3cfd5454a11e5813e72179b4b7d
Eli Elder <eli-elder@hotmail.com>
2019-04-05 00:21:54 -0700

    localhost hard coded into axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,7 +29,7 @@ class App extends Component<Props, State> {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/colors/')
+    axios.get('/colors/')
       .then((res) => {
         console.log(res)
         this.setState({
